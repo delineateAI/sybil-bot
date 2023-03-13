8767bfd4123a0247d01af023cdb34e858571601c
loross <loross@umich.edu>
2023-03-12 22:55:16 -0400

    cleaned up some things

--- src/agent.py
+++ src/agent.py
@@ -34,44 +34,9 @@ def is_eoa(w3: Web3, address: str) -> bool:
 
 known_airdrop_addresses = set()
 num_transactions_contract_address = {}
+known_ignore_list= set()
 
 def handle_transaction(w3, transaction_event):
-
-    ##TRANSACTION FLOW over time
-        #no clue yet how to do this -- read papers
-
-    ##AIRDROP CLAIM
-    #so we are getting any transaction in a block
-    #this transaction could be a token airdrop transfer meaning from a token's smart contract to an EOA (externally owned account)
-        #how do we know it it is an airdrop transaction? What are the identifying features
-            #should we make a list of known token contract addresses on Ethereum ?
-    # |
-    # | these steps may be both a part of determining if its an airdrop transaction
-    # |
-    #IF this is indeed something to do with an airdrop
-        #look for EOA calling claim function OR
-        #EOA recieving a lot of tokens in this transaction
-
-    #find claim function in signature hash
-        #once you have a human readable function using rainbow table check to see if in list of airdrop functions
-        # use below to monitor this function or something idk
-        #https://github.com/arbitraryexecution/forta-bot-templates/tree/main/src/monitor-function-calls (can be used to find function calls to a contract)
-
-
-
-    #so we know this is an airdrop claim transaction and we know which functions have been called in the token contract
-        #from this determine the chances this is an airdrop hunter (size trasnaction, other features -- LOT OF WORK HERE)
-
-    # -- can we use other forta bots to analyze wallets/transaction flowas over time/ect?
-
-
-    # find out if this is a token airdrop trasnaction of some sort
-    # Criteria for identifying airdrops from transactions:
-        # TODO identify as being a token transfer
-        # contract address exists
-        # several transactions with same contract address in timeframe
-        # functionName claim on rainbowtable lookup
-            # is the token address just the to_address or something else? TODO
 
     findings = []
     from_address = transaction_event.from_
@@ -79,10 +44,6 @@ def handle_transaction(w3, transaction_event):
 
     eoa_sender = is_eoa(w3, from_address)
     eoa_reciever =  is_eoa(w3, to_address)
-
-    def checkSybil():
-        # TODO
-        pass
 
     if not eoa_sender:
         # contract address sender, possibly an airdrop
@@ -117,13 +78,28 @@ def handle_transaction(w3, transaction_event):
     if input_data is None:
         return findings
 
+    # Determine if tx is potential claim, confirmed claim or confirmed token transfer:
+    #
+    # if confirmed token transfer: ERC-20 Token transfer -> if token is on known airdrop list -> checkSybil()
+    #
+    # if confirmed_claim -> storeClaimer()
+    # 
+    # if potential claim (function sends ERC-20 to address for nothing in return) -> determineClaim()
+    # - find claim function in signature hash and ERC-20 address and store in confirmed
 
+
+    # some other useful comments:
+    # once you have a human readable function using rainbow table check to see if in list of airdrop functions
+    # use below to monitor this function or something idk
+    #https://github.com/arbitraryexecution/forta-bot-templates/tree/main/src/monitor-function-calls (can be used to find function calls to a contract)
+    #from this determine the chances this is an airdrop hunter (size trasnaction, other features -- LOT OF WORK HERE)
+    # -- can we use other forta bots to analyze wallets/transaction flowas over time/ect?
+
+def determineClaim(tx):
     #see if the transaction was signed by a token contract function like claim
 
-
-    #we want to look at map of all addresses involved most liley
+    #we want to look at map of all addresses involved most likely
     # addresses = transaction_event.addresses
-
 
     # #to filter and decode function calls in the transaction or traces
     # erc20_token_address = '0x123abc'
@@ -131,7 +107,13 @@ def handle_transaction(w3, transaction_event):
     # transfers = transaction_event.filter_log(transfer_event_abi, erc20_token_address)
     # print(f'found {transfers.length} transfer events')
 
+def checkSybil():
+    # TODO
+    pass
 
+def storeClaimer():
+    # if identified claim function is called, store the wallet address for sybil analysis
+    pass
 
 def initialize():
     # do some initialization on startup e.g. fetch data
