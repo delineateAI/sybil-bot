8e7366e78089058edda0f7e3046c7ab50474a50b
asafdari-boop <asafdari@umich.edu>
2023-03-15 22:30:38 -0400

    TODOs completed

--- src/agent2.py
+++ src/agent2.py
@@ -2,8 +2,25 @@ from forta_agent import Finding, FindingType, FindingSeverity, get_json_rpc_url
 from hexbytes import HexBytes
 from web3 import Web3
 import defaultdict
+from datetime import datetime, timedelta
+
 import logging
 
+ERC_20_TRANSFER_EVENT_ABI = '{"name":"Transfer","type":"event","anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}]}'
+ERC_20_TRANSFER_FROM_FUNCTION_ABI = '{"name":"transferFrom","type":"function","constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"outputs":[],"payable":false,"stateMutability":"nonpayable"}'
+
+# findings.append(Finding({
+#     'name': 'Large Tether Transfer',
+#     'description': f'{amount} USDT transferred',
+#     'alert_id': 'FORTA-7',
+#     'type': FindingType.Info,
+#     'severity': FindingSeverity.Info,
+#     'metadata': {
+#         'from': event['args']['from'],
+#         'to': event['args']['to'],
+#         'amount': amount
+#     }
+# }))
 web3 = Web3(Web3.HTTPProvider(get_json_rpc_url()))
 
 known_airdrop_addresses = set()
@@ -29,7 +46,7 @@ def handle_transaction(w3, transaction_event):
     # Check if the token address is in the ignore list
     if erc20_address in known_ignore_list:
         return findings
-    
+
     # Get the input data of the transaction
     transaction_data = transaction_event.transaction.data
 
@@ -46,26 +63,51 @@ def handle_transaction(w3, transaction_event):
     # This is the address the token will be sent to by the token contract transfer function
     # right now we are assuming
     # need to figure out how to parse without the abi -- to get abi we need etherscan
-    # TODO: See how to actually get argument (remove leading 0s)
-    recipient_address = "0x" + transaction_data[10:74]
+    # int removes leading 0s if transaction_Data is a string
+    recipient_address = "0x" + int(transaction_data[10:74])
 
-    # TODO: assure recipient address is NOT an exchange wallet!!!
-    return checkSybil(recipient_address, erc20_address)
+    # TODO: Check if exchnage wallet (isn't this the if check in the sybil attack func)
+    #what alert id do we raise, severity, ect
+    if(checkSybil(recipient_address, erc20_address)):
+        findings.append( Finding({
+        'name': 'Sybil Attack',
+        'description': f'This wallet may be involved in a Sybil Attack for token {erc20_address}',
+        'alert_id': 'FORTA-7',
+        'type': FindingType.Info,
+        'severity': FindingSeverity.Info,
+        'metadata': {
+            'from': erc20_address,
+            'to': recipient_address
+        }}))
+    return findings
+
 
 
 # check for sybil attacks given an airdrop from_address
-# construct a graph of to_address transactions for every to_address
-# see if there is any particular account that receives transfers from more than 5 to_addresses
-def checkSybil(sender_address, erc20_address):
-
+# see if there is any particular account that receives transfers
+def checkSybil(transaction_event, sender_address, erc20_address):
+     # filter the transaction logs for USDT Transfer events
     # find all erc20_address transactions going TO the sender_address in the last week
     # start by pulling 50 transactions and then check the timestamp on the oldest
+    token_transfer_events = transaction_event.filter_log(ERC_20_TRANSFER_EVENT_ABI, erc20_address)
+    # find all erc20_address transactions going FROM the erc20_address to sender_address in the last week
+    # num_transactions = 0
+    week_ago = datetime.now() - timedelta(weeks=1)
+    for event in token_transfer_events:
+        timestamp = datetime.fromtimestamp(event['timestamp'])
+        if event['args']['from'] == erc20_address and \
+            event['args']['to'] == sender_address and \
+            timestamp >= week_ago:
+            num_transactions += 1
 
-    # if more than 5 and less than 500, throw alert 
-    return true
-    # 500 is probably larger than any sybil attack, in which case its an exchange wallet
-    return false
-    
+    # if more than 5 and less than 500, throw alert
+    if num_transactions > 5 and num_transactions < 500:
+        return True
+    else:
+        # 500 is probably larger than any sybil attack, in which case its an exchange wallet
+        return False
+
+
 
 def is_eoa(w3: Web3, address: str) -> bool:
     """
